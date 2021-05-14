6f83fc220b19276b823a1a589a131a07d48b67ac
OrIdow6 <68304414+OrIdow6@users.noreply.github.com>
2021-05-14 02:33:52 -0700

    Branding

--- pipeline.py
+++ pipeline.py
@@ -154,7 +154,7 @@ class MaybeSendDoneToTracker(SendDoneToTracker):
 
 CWD = os.getcwd()
 PIPELINE_SHA1 = get_hash(os.path.join(CWD, 'pipeline.py'))
-LUA_SHA1 = get_hash(os.path.join(CWD, 'bintray.lua'))
+LUA_SHA1 = get_hash(os.path.join(CWD, 'tinkercad.lua'))
 
 def stats_id_function(item):
     d = {
@@ -174,7 +174,7 @@ class WgetArgs(object):
             '-nv',
             '--content-on-error',
             '--load-cookies', 'cookies.txt',
-            '--lua-script', 'bintray.lua',
+            '--lua-script', 'tinkercad.lua',
             '-o', ItemInterpolation('%(item_dir)s/wget.log'),
             '--no-check-certificate',
             '--output-document', ItemInterpolation('%(item_dir)s/wget.tmp'),
@@ -230,10 +230,10 @@ class WgetArgs(object):
 # This will be shown in the warrior management panel. The logo should not
 # be too big. The deadline is optional.
 project = Project(
-    title = 'bintray',
+    title = 'tinkercad',
     project_html = '''
     <img class="project-logo" alt="logo" src="https://wiki.archiveteam.org/images/Archiveteamsmall.png?959ea" height="50px"/>
-    <h2>Super Mario Maker Bookmarks <span class="links"><a href="https://supermariomakerbookmark.nintendo.net/">Website</a> &middot; <a href="http://tracker.archiveteam.org/super-mario-maker-bookmarks/">Leaderboard</a></span></h2>
+    <h2>Tinkercad <span class="links"><a href="https://www.tinkercad.com/">Website</a> &middot; <a href="http://tracker.archiveteam.org/tinkercad/">Leaderboard</a></span></h2>
     ''',)
 
 pipeline = Pipeline(
@@ -241,7 +241,7 @@ pipeline = Pipeline(
     GetItemFromTracker('http://{}/{}/multi={}/'
         .format(TRACKER_HOST, TRACKER_ID, MULTI_ITEM_SIZE),
         downloader, VERSION),
-    PrepareDirectories(warc_prefix='bintray'),
+    PrepareDirectories(warc_prefix='tinkercad'),
     WgetDownload(
         WgetArgs(),
         max_tries=1,
