547fa5e5ce5d8ba5ab8833a624969a671567236a
arkiver <arkiver@protonmail.com>
2021-05-18 00:32:41 +0200

    Fix logo, --domains argument, style.

--- pipeline.py
+++ pipeline.py
@@ -185,7 +185,7 @@ class WgetArgs(object):
             '--page-requisites',
             '--timeout', '30',
             '--tries', 'inf',
-            '--domains', 'voat.co',
+            '--domains', 'tinkercad.com',
             '--span-hosts',
             '--waitretry', '30',
             '--warc-file', ItemInterpolation('%(item_dir)s/%(warc_file_base)s'),
@@ -208,9 +208,9 @@ class WgetArgs(object):
                 wget_args.extend(['--warc-header', 'tinkercad-user: ' + item_value])
                 wget_args.append(f'https://www.tinkercad.com/users/{item_value}')
             elif item_type == "submission":
-                 #wget_args.extend(['--warc-header', 'tinkercad-submission: ' + item_value])
-                 #wget_args.append(f'https://www.tinkercad.com/things/{item_value}')
-                 item_names_to_submit.remove(item_name)
+                #wget_args.extend(['--warc-header', 'tinkercad-submission: ' + item_value])
+                #wget_args.append(f'https://www.tinkercad.com/things/{item_value}')
+                item_names_to_submit.remove(item_name)
             elif item_type == "asset":
                 assert item_value.startswith("http")
                 wget_args.extend(['--warc-header', 'tinkercad-asset: ' + item_value])
@@ -237,7 +237,7 @@ class WgetArgs(object):
 project = Project(
     title = 'tinkercad',
     project_html = '''
-    <img class="project-logo" alt="logo" src="https://wiki.archiveteam.org/images/Archiveteamsmall.png?959ea" height="50px"/>
+    <img class="project-logo" alt="logo" src="https://wiki.archiveteam.org/images/6/66/Tinkercad_icon.png" height="50px"/>
     <h2>Tinkercad <span class="links"><a href="https://www.tinkercad.com/">Website</a> &middot; <a href="http://tracker.archiveteam.org/tinkercad/">Leaderboard</a></span></h2>
     ''',)
 
